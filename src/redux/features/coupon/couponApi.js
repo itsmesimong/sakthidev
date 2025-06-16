36fa37b308d96261e8fb6eb8d05f3f2c06b60238
Simon G <142384587+itsmesimong@users.noreply.github.com>
2025-06-16 18:29:51 +0530

    Update couponApi.js

--- src/redux/features/coupon/couponApi.js
+++ src/redux/features/coupon/couponApi.js
@@ -5,7 +5,7 @@ export const authApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     // get offer coupon
     getOfferCoupons: builder.query({
-      query: () => `http://10.160.21.87:7000/api/coupon`,
+      query: () => `https://romantic-gates.203-101-41-20.plesk.page/api/coupon`,
       providesTags:['Coupon'],
       keepUnusedDataFor: 600,
     }),
