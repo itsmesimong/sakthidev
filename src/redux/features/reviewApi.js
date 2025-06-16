8cf1be5ee16ca87a8ff99b6edbdda96b23df8006
Simon G <142384587+itsmesimong@users.noreply.github.com>
2025-06-16 18:27:58 +0530

    Update reviewApi.js

--- src/redux/features/reviewApi.js
+++ src/redux/features/reviewApi.js
@@ -5,7 +5,7 @@ export const reviewApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     addReview: builder.mutation({
       query: (data) => ({
-        url: "http://10.160.21.87:7000/api/review/add",
+        url: "https://romantic-gates.203-101-41-20.plesk.page/api/review/add",
         method: "POST",
         body: data,
       }),
